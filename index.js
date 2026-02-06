e5646154f035391fa9fea08ff0709562502b5dca
Nehal Kadyan <89070793+nehalkadyan@users.noreply.github.com>
2026-02-06 19:36:19 +0530

    Update CORS origin and methods in index.js

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-    origin : "https://linkedin-frontend-theta.vercel.app/",
+    origin : "https://linkedin-frontend-theta.vercel.app/signin",
+    methods : ["GET", "POST", "PUT", "DELETE"],
     credentials : true
 }))
 // mongodb -> connection string
